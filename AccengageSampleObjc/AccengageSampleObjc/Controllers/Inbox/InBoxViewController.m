6c0e3a88abd231562fe09520e2d7c76a136951e8
Romain Dubreucq <rdubreucq@accengage.com>
2017-07-03 10:01:50 +0200

    add markAsDisplayed method

--- AccengageSampleObjc/AccengageSampleObjc/Controllers/Inbox/InBoxViewController.m
+++ AccengageSampleObjc/AccengageSampleObjc/Controllers/Inbox/InBoxViewController.m
@@ -249,6 +249,7 @@
   NSInteger index = indexPath.row;
   [self.inbox obtainMessageAtIndex:index loaded:^(BMA4SInBoxMessage *message, NSUInteger requestedIndex) {
     if (index == requestedIndex && !message.isDisplayed) {
+      [message markAsDisplayed];
       [message trackDisplay];
     }
   } onError:nil];
